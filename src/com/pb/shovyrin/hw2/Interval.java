71cc941dc0cffbb14833c9d4fc1a4871481ff467
Yaroslav <shovyrin.yaroslav@gmail.com>
2021-10-13 08:38:11 +0300

    Second homework

---
+++ src/com/pb/shovyrin/hw2/Interval.java
@@ -0,0 +1,25 @@
+package com.pb.shovyrin.hw2;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        int x;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите любое число");
+        x = in.nextInt();
+
+        if(x > 100){
+            System.out.println("Число не входит в промежуток");
+        }else if(x >= 51){
+            System.out.println("Число входит в промежуток [51 - 100]");
+        }else if(x >= 36){
+            System.out.println("Число входит в промежуток [36 - 50]");
+        }else if(x >= 15){
+            System.out.println("Число входит в промежуток [15 - 35]");
+        }else if(x >= 0){
+            System.out.println("Число входит в промежуток [0 -14]");
+        }
+    }
+}
