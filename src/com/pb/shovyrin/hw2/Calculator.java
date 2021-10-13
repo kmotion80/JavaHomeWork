06a2d4d53263c503e25ae181f99e94407f16e0e5
Yaroslav <shovyrin.yaroslav@gmail.com>
2021-10-13 08:38:11 +0300

    Second homework

---
+++ src/com/pb/shovyrin/hw2/Calculator.java
@@ -0,0 +1,50 @@
+package com.pb.shovyrin.hw2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x, y;
+        double result = 0.0;
+        String sign;
+        Boolean noError = true;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Калькулятор v 1.0");
+        System.out.println("Введите число х: ");
+        x = in.nextInt();
+
+        System.out.println("Введите число y: ");
+        y = in.nextInt();
+
+        System.out.println("Введите знак арифметической операции: ");
+        sign = in.next();
+
+        switch (sign) {
+            case "+":
+                result = x + y;
+                break;
+            case "-":
+                result = x - y;
+                break;
+            case "*":
+                result = x * y;
+                break;
+            case "/":
+                if (y != 0) {
+                    result = (double)x / y;
+                } else {
+                    noError = false;    //ставлю значение ошибки и вывод результата не обязателен
+                    System.out.println("Деление на 0 запрещено.");
+                }
+                break;
+            default:
+                System.out.println("Не поддерживаемая операция");
+                noError = false;
+        }
+        //Если нет ошибок выводим результат
+        if (noError) {
+            System.out.println("Результа вычисления: " + result);
+        }
+    }
+}
